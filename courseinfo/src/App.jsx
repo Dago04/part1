0f84604aef7775586df7ed901052d4eeced6e98b
Dagoberto Salas <DagoSalas1999@gmail.com>
2024-07-19 20:46:44 -0600

    Total create

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -14,6 +14,14 @@ const Content = ({ part, exercises }) => {
   )
 }
 
+const Total = ({ total }) => {
+  return (
+    <p>
+      Number of exercises {total}
+    </p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development';
   const part1 = 'Fundamentals of React';
@@ -26,6 +34,10 @@ const App = () => {
   return (
     <>
       <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </>
   )
 }
